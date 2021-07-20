332e8205e243b4ecb0d8c0d8b95d4926969ea021
Meisam <msm.kabiri91@gmail.com>
2021-07-20 12:25:13 +0200

    Modifiying the Publisher

--- src/wireless_publish.cpp
+++ src/wireless_publish.cpp
@@ -14,7 +14,26 @@
 //#include <gazebo/gazebo_client.hh>
 //gazebo::msgs::WirelessNodes NodesMsg;
 
-  int nthSubstr(int n, const std::string& s,
+class rss_data_handler 
+{
+
+public:
+  rss_data_handler() {}
+  ~rss_data_handler() {}
+
+private:
+  ros::Publisher rss_pub;
+  gazebo::transport::SubscriberPtr gazebo_sub;
+
+public:
+  void open(gazebo::transport::NodePtr node, ros::NodeHandle n);
+  void receiver_subs(ConstWirelessNodesPtr &msg);
+  void publish_rss_data(float a1, float a2, float a3);
+
+};
+
+
+int nthSubstr(int n, const std::string& s,
               const std::string& p) {
               size_t i = s.find(p);     // Find the first occurrence
 
@@ -29,23 +48,31 @@
 }
 
 
+void rss_data_handler::publish_rss_data(float a1, float a2, float a3){
 
-float sig_level1;
-float sig_level2;
-float sig_level3;
-void receiver_subs(ConstWirelessNodesPtr &msg)
-{
+  gazebo_wireless::Rss_level NodeMsg;
+  NodeMsg.signal_level1 = a1;
+  NodeMsg.signal_level2 = a2;
+  NodeMsg.signal_level3 = a3;
+  rss_pub.publish(NodeMsg);
+}
+
+void rss_data_handler::receiver_subs(ConstWirelessNodesPtr &msg)
+{  
     std::string raw_msg = msg->DebugString();
     size_t occur1 = raw_msg.find("signal_level:");
     size_t occur2 = raw_msg.find("signal_level:", occur1+1);
     size_t occur3 = raw_msg.find("signal_level:", occur2+1);
     //size_t occur1 = 43; size_t occur2 = 121; size_t occur3 = 201;  
-
-
     std::string sig_lev_str1 = raw_msg.substr(occur1+13, 15);
     std::string sig_lev_str2 = raw_msg.substr(occur2+13, 15);
     std::string sig_lev_str3 = raw_msg.substr(occur3+13, 15);
     
+
+    float sig_level1;
+    float sig_level2;
+    float sig_level3;
+
     sig_level1 = std::stof(sig_lev_str1);
     sig_level2 = std::stof(sig_lev_str2);
     if (occur3 <1000){
@@ -55,56 +82,42 @@ void receiver_subs(ConstWirelessNodesPtr &msg)
     }
     
     std::cout << sig_lev_str1 <<"\n"<<sig_lev_str2 <<"\n"<<sig_lev_str3 <<"\n-----------------\n" <<std::endl;
+    this->publish_rss_data(sig_level1, sig_level2, sig_level3);
 
 }
+
+void rss_data_handler::open(gazebo::transport::NodePtr node, ros::NodeHandle n) {
+
+  // Subscribe to a topic
+  gazebo_sub = node->Subscribe("/gazebo/default/iris_opt_flow/wirelessReceiver/link/wirelessReceiver/transceiver", 
+                                                      &rss_data_handler::receiver_subs, this);
+  rss_pub = n.advertise<gazebo_wireless::Rss_level>("/receiver_rss", 300);
+}
+
 
 int main(int _argc, char **_argv)
 {
-
+  // gazebo setup
   gazebo::client::setup(_argc, _argv);
   gazebo::transport::NodePtr node(new gazebo::transport::Node());
-  
-  node->Init ();
-  // start transportation
-  //gazebo::transport::run();
+  node->Init();
 
-  // Subscribe to a topic
-  gazebo::transport::SubscriberPtr sub_2 = node->Subscribe("/gazebo/default/iris_opt_flow/wirelessReceiver/link/wirelessReceiver/transceiver", receiver_subs);
-  //publish on Gazebo
-  //gazebo::transport::PublisherPtr sensor_pub = node->Advertise <gazebo::msgs::WirelessNodes>("/sensor/receiver", 1000, (1.0));
-  //sensor_pub -> Publish(NodesMsg);
-  //sensor_msgs::ChannelFloat32 Rec_msgs ;
-
-
-
-  //publish on Ros
-
-
-
-  ros::init(_argc, _argv, "talker");
+  //ros setup
+  ros::init(_argc, _argv, "rss_handler");
   ros::NodeHandle n;
-  ros::Publisher chatter_pub = n.advertise<gazebo_wireless::Rss_level>("/receiver_rss", 300);
-  gazebo_wireless::Rss_level NodeMsg;
-  NodeMsg.signal_level1 = sig_level1;
-  NodeMsg.signal_level2 = sig_level2;
-  NodeMsg.signal_level3 = sig_level3;
-  
 
-  ros::Rate loop_rate(10);
+  rss_data_handler rss_data_handle_obj;
+  rss_data_handle_obj.open(node, n);
 
-  int count = 0;
-    while (ros::ok())
-   {
-  chatter_pub.publish(NodeMsg);
-  ros::spinOnce(); 
-  loop_rate.sleep();
-  ++count;
-   }
- 
   // shutting down
-  while (true)
+  while (ros::ok()){
     gazebo::common::Time::MSleep(10);
-
+  }
   // Make sure to shut everything down.
-   gazebo::client::shutdown();
+  ros::shutdown();
+  gazebo::client::shutdown();
+
 }
+
+
+// d0 = -25 db alpha = 6
